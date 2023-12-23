6eba28e3cb1242cfc2a29096dcd84b0a6a6a3df8
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2023-12-23 19:27:49 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+let name=document.getElementById("fname").value;
+let con=document.getElementById("contact").value;
+let email=document.getElementById("email").value;
+function fun(){
+
+    console.log(name.value);
+     console.log(con.value);
+     console.log(email.value);
+}
